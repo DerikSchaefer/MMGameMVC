385fe7e29e573cccbe3317fb692d20f4e53340ae
Derik <Derik.p.schaefer@gmail.com>
2023-01-22 08:57:48 -0800

    Add project files.

---
+++ Controllers/MastermindController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class MastermindController : Controller
+{
+    private MastermindModel _model;
+    public MastermindController()
+    {
+        _model = new MastermindModel();
+    }
+
+    public IActionResult Index()
+    {
+        var model = new MastermindModel();
+        return View(model);
+    }
+
+    [HttpPost]
+
+    public IActionResult Guess(int[] guess)
+    {
+        _model.TurnsRemaining--;
+        _model.GuessHistory.Add(guess);
+
+        int correctDigits = 0;
+        int correctPositions = 0;
+        for (int i = 0; i < guess.Length; i++)
+        {
+            if (guess[i] == _model.Solution[i])
+            {
+                correctPositions++;
+            }
+            else if (_model.Solution.Contains(guess[i]))
+            {
+                correctDigits++;
+            }
+        }
+        ViewData["CorrectDigits"] = correctDigits;
+        ViewData["CorrectPositions"] = correctPositions;
+
+        return View("Index", _model);
+    }
+
+    public IActionResult NewGame()
+    {
+        _model = new MastermindModel();
+        return RedirectToAction("Index");
+    }
+}
---
+++ Models/MastermindModel.cs
@@ -0,0 +1,25 @@
+﻿public class MastermindModel
+{
+    public int[] Solution { get; set; }
+    public int TurnsRemaining { get; set; }
+    public List<int[]> GuessHistory { get; set; }
+
+    public MastermindModel()
+    {
+        TurnsRemaining = 10;
+        GuessHistory = new List<int[]>();
+
+        using (HttpClient client = new HttpClient())
+        {
+            string apiUrl = "https://www.random.org/integers/?num=4&min=1&max=6&col=1&base=10&format=plain&rnd=new";
+            string apiResponse = client.GetStringAsync(apiUrl).Result;
+            string[] responseArray = apiResponse.Split(new[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            int[] SolutionArray = new int[responseArray.Length];
+            for (int i = 0; i < responseArray.Length; i++)
+            {
+                SolutionArray[i] = int.Parse(responseArray[i]);
+            }
+            Solution = SolutionArray;
+        }
+    }
+}
